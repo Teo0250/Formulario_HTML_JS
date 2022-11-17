ce9caf82ecd0ee92bfcd60bc9b722fc839002389
Matheus Medeiros <mf.matheus00@gmail.com>
2022-11-17 16:53:57 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false;
+
+function validaNome(nomeCompleto){
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - Conta: <b>${numeroContaBeneficiario.value}</b>`
+
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
+        const containerMessageSuccess = document.querySelector('.success-message');
+        containerMessageSuccess.innerHTML - mensagemSucesso;
+        containerMessageSuccess.style.display = 'block';
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+    } else {
+        nomeBeneficiario.style.border = '1px solid red'
+        document.querySelector('.error-message').style.display = 'block';
+    }
+    //if (!validaNome(nomeBeneficiario.value)) (validaNome(nomeBeneficiario.value) === false)
+})
+
+nomeBeneficiario.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+    formEValido = validaNome(e.target.value)
+
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error');
+        //nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+})
+
